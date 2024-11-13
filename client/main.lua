d47705e8ce17bdaff23ac9a116ca03d6c3200d31
FriskyXE <supanat.tinz@gmail.com>
2024-11-13 18:55:45 +0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,8 +1,6 @@
--- client.lua
 local Fonts = {}
 local fontsLoaded = false
 
--- ฟังก์ชันสำหรับโหลดฟอนต์ทั้งหมดใน config
 function LoadCustomFonts()
     if not fontsLoaded then
         for _, font in ipairs(Config.CustomFonts) do
@@ -13,21 +11,14 @@ function LoadCustomFonts()
     end
 end
 
--- ฟังก์ชันสำหรับดึง fontId โดยใช้ชื่อฟอนต์
 function GetFontId(fontName)
-    return Fonts[fontName] or 0 -- คืนค่า fontId หรือ 0 ถ้าไม่พบฟอนต์
-end
+    return Fonts[fontName] or 0 
+end exports("GetFontId", GetFontId)
 
--- Export ฟังก์ชัน GetFontId สำหรับใช้งานนอกสคริปต์นี้
-exports("GetFontId", GetFontId)
-
--- ใช้ CreateThread เพื่อโหลดฟอนต์เมื่อสคริปต์ client-side โหลดขึ้นมา
 CreateThread(function()
-    print('CustomFont is loaded!')
     LoadCustomFonts()
 end)
 
--- ใช้ Event เพื่อโหลดฟอนต์ซ้ำเมื่อผู้เล่นโหลดเสร็จสมบูรณ์
 AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
     LoadCustomFonts()
 end)
