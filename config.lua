1c210093720be9aa3eee84dfff7363a58085f0b1
FriskyXE <supanat.tinz@gmail.com>
2024-11-13 19:08:27 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,6 @@
 Config = {}
 
 Config.CustomFonts = {
-    {name = "CustomFont1", file = "customfont1"},
-    {name = "CustomFont2", file = "customfont2"},
-    -- เพิ่มฟอนต์อื่น ๆ ที่ต้องการได้ที่นี่
+    {name = "Supermarket", file = "supermarket"},
+    -- {name = "CustomFont", file = "customfont"},
 }
